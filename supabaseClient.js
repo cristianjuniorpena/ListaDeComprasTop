21771197b9d6e07a20dde51d9a555737c225b2d6
cristianjuniorpena <cristianjuniorpena@gmail.com>
2026-03-03 08:58:14 -0300

    comit

---
+++ supabaseClient.js
@@ -0,0 +1,6 @@
+import {createClient} from 
+
+export const supabase = createClient (
+
+    eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVxcWV5a2VqY2Vkb2RkdHV2d3JpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIxMTE4NDgsImV4cCI6MjA4NzY4Nzg0OH0.Lq32WZRaUEvaBztEnevRkpfU6sxDBBJQnM8QELwiJ8w
+)
