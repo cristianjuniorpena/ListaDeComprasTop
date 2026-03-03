8e139d9b2d52c23042c7892db38fad4a568c9ce6
cristianjuniorpena <cristianjuniorpena@gmail.com>
2026-03-03 09:02:13 -0300

    ok

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,6 +1,6 @@
-import {createClient} from 
+import {createClient} from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
 export const supabase = createClient (
-
-    eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVxcWV5a2VqY2Vkb2RkdHV2d3JpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIxMTE4NDgsImV4cCI6MjA4NzY4Nzg0OH0.Lq32WZRaUEvaBztEnevRkpfU6sxDBBJQnM8QELwiJ8w
+    "https://eqqeykejcedoddtuvwri.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVxcWV5a2VqY2Vkb2RkdHV2d3JpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIxMTE4NDgsImV4cCI6MjA4NzY4Nzg0OH0.Lq32WZRaUEvaBztEnevRkpfU6sxDBBJQnM8QELwiJ8w"
 )
